6c29f400a583a773b145b417278786dbe7598f65
ksy <tndud4615@gmail.com>
2026-02-02 10:41:43 +0900

    2026-02-02 초기화

--- board-front/src/router/index.js
+++ board-front/src/router/index.js
@@ -7,18 +7,6 @@ const router = createRouter({
       path: '/',
       component: () => import('../views/ListPage.vue'),
     },
-    {
-      path: '/write',
-      component: () => import('../views/PostPage.vue'),
-    },
-    {
-      path: '/detail/:id',
-      component: () => import('../views/DetailPage.vue'),
-    },
-    {
-      path: '/mod/:id',
-      component: () => import('../views/PostPage.vue'),
-    },
   ],
 })
 
