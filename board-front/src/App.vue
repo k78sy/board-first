302e68f540d3b3bd2deb445eece51e05012ca29c
ksy <tndud4615@gmail.com>
2026-02-02 14:28:05 +0900

    2026-02-02 오후 초기화

--- board-front/src/App.vue
+++ board-front/src/App.vue
@@ -3,12 +3,4 @@
 </script>
 
 <template>
-    <nav>
-        <router-link to="/">HOME</router-link>
-        <router-link to="/write">WRTIE</router-link>
-    </nav>
-
-    <main>
-        <router-view></router-view>
-    </main>
 </template>
