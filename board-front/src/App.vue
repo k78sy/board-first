afe75b80968ebb3e55d762fd7a504e3e8dde9d1a
ksy <tndud4615@gmail.com>
2026-02-02 18:11:13 +0900

    초기화!!

--- board-front/src/App.vue
+++ board-front/src/App.vue
@@ -3,11 +3,4 @@
 </script>
 
 <template>
-    <nav>
-        <router-link to="/">HOME</router-link>
-        <router-link to="/write">WRITE</router-link>
-    </nav>
-    <main>
-        <router-view />
-    </main>
 </template>
